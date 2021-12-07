8f5a08fd6c56e0891195f84b6206d6be3b94a039
nodemtiwa <95690350+nodemtiwa@users.noreply.github.com>
2021-12-07 13:10:55 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,21 +1,9 @@
 #!/usr/bin/python
 
-USERNAME = ''
-PASSWORD = ''
+USERNAME = 'Nodem Tiwa'
+PASSWORD = 'MATHEMATIQUE2001@'
 
 COURSES = [
-    'it-security-foundations-core-concepts',
-    'charlene-li-on-digital-leadership',
-    # 'fred-kofman-on-managing-conflict',
-    # 'reid-hoffman-and-chris-yeh-on-creating-an-alliance-with-employees',
-    # 'bill-george-on-self-awareness-authenticity-and-leadership',
-    # 'sallie-krawcheck-on-risk-taking',
-    # 'betsy-corcoran-on-choosing-the-right-technology-for-your-school',
-    # 'betty-liu-on-career-success',
-    # 'j-t-o-donnell-on-making-recruiters-come-to-you',
-    # 'gary-hamel-on-busting-bureaucracy',
-    # 'shane-snow-on-storytelling',
-    # 'john-maeda-on-design-business-and-inclusion',
-    # 'esther-dyson-on-cultivating-health-at-scale',
-    # 'influencer-interview-oprah-winfrey'
+    'using-night-light?autoAdvance=true&autoSkip=false&autoplay=true&resume=true',
+    'lighting-your-buildings?autoAdvance=true&autoSkip=false&autoplay=true&resume=true',
 ]
